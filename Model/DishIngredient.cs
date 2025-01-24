cec796ebce52db900cb95137b50930dffe79cf7c
oliwier <olik03.2003@wp.pl>
2025-01-25 00:07:46 +0100

    improved post dishes with dtos

--- Model/DishIngredient.cs
+++ Model/DishIngredient.cs
@@ -3,8 +3,8 @@ namespace Food_Creator.Model;
 public class DishIngredient
 {
     public int DishId { get; set; }
-    public Dish Dish { get; set; }   // Relacja do Dish
+    public Dish? Dish { get; set; }   // Relacja do Dish
     public int IngredientId { get; set; }
-    public Ingredient Ingredient { get; set; }   // Relacja do Ingredient
+    public Ingredient? Ingredient { get; set; }   // Relacja do Ingredient
     public float Quantity { get; set; }
 }
