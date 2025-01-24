a4f5d865b7e70fccf362babc5cb9d9cec76843ca
Magda <magsxl09@gmail.com>
2025-01-24 18:35:15 +0100

    dodaj put i delete metody

--- Model/Ingredient.cs
+++ Model/Ingredient.cs
@@ -6,5 +6,5 @@ public class Ingredient
     public string Name { get; set; }
     public string Url { get; set; }
     public float Price { get; set; }
-    public ICollection<DishIngredient> DishIngredients { get; set; }
+    public ICollection<DishIngredient>? DishIngredients { get; set; }
 }
