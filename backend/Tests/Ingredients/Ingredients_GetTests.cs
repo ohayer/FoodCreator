3c19da98589f3af3310cced3a5f40ef6d6192c06
magda-rgb <magsxl09@gmail.com>
2025-06-21 10:56:09 +0200

    Adjust tests

--- backend/Tests/Ingredients/Ingredients_GetTests.cs
+++ backend/Tests/Ingredients/Ingredients_GetTests.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Food_Creator.controller;
@@ -47,5 +47,52 @@ public class Ingredients_GetTests
         var dtos     = Assert.IsAssignableFrom<IEnumerable<IngredientDto>>(ok.Value);
 
         Assert.Empty(dtos);
+    }
+    
+    /// <summary>
+    /// Sprawdzenie czy obraz instnieje
+    /// </summary>
+    [Fact]
+    public async Task GetImage_ExistingIngredientWithImage_ReturnsFile()
+    {
+        var ctx = TestDbContextFactory.Create();
+        ctx.Ingredients.Add(new Ingredient { Name = "IngWithImage", Price = 1.0f, Image = new byte[] { 1, 2, 3 } });
+        await ctx.SaveChangesAsync();
+
+        var ctrl = new IngredientsController(ctx);
+        var ingredient = ctx.Ingredients.First();
+        var result = await ctrl.GetImage(ingredient.IngredientId);
+
+        Assert.IsType<FileContentResult>(result);
+    }
+    
+    /// <summary>
+    /// Sprawdzenie czy obraz nie istnieje
+    /// </summary>
+    [Fact]
+    public async Task GetImage_IngredientWithoutImage_ReturnsNotFound()
+    {
+        var ctx = TestDbContextFactory.Create();
+        ctx.Ingredients.Add(new Ingredient { Name = "IngWithNoImage", Price = 1.0f });
+        await ctx.SaveChangesAsync();
+
+        var ctrl = new IngredientsController(ctx);
+        var result = await ctrl.GetImage(ctx.Ingredients.First().IngredientId);
+
+        Assert.IsType<NotFoundResult>(result);
+    }
+    
+    /// <summary>
+    /// Sprawdzenie nieistniejącego składnika
+    /// </summary>
+    [Fact]
+    public async Task GetImage_NonExistingIngredient_ReturnsNotFound()
+    {
+        var ctx = TestDbContextFactory.Create();
+        var ctrl = new IngredientsController(ctx);
+
+        var result = await ctrl.GetImage(999);
+
+        Assert.IsType<NotFoundResult>(result);
     }
 }
